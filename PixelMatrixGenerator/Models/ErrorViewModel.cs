92be94bf9131d633ea58e5159705f75a3ae5aa3c
Joshua Ghezzi <joshuaghezzi@outlook.com>
2018-04-29 12:00:20 +1200

    Initial Commit

---
+++ PixelMatrixGenerator/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PixelMatrixGenerator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
